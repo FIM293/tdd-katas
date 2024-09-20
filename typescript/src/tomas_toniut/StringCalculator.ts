bec6c93401595f758760fcd9f15b50708f7c643b
tomastoniut01 <tomytoniut@gmail.com>
2024-09-20 10:39:16 -0300

    feat[StringCalculator]: el cuarto test ha pasado

--- typescript/src/tomas_toniut/StringCalculator.ts
+++ typescript/src/tomas_toniut/StringCalculator.ts
@@ -4,8 +4,8 @@ export class StringCalculator {
   add(input:string) {
     if (input === '')
       return 0;
-    const numbers = input.split(',');
-
+    const numbers = input.split(/[, \n]+/);
+    console.log(numbers)
     //el reduce toma la variable 'sum' como acumuladr y 'num' como el valor actual de la iteración. El cero al final es el valor inicial de acumulador. 
     const result = numbers.reduce((sum, num) => sum + parseInt(num), 0)
     return Number (result);
