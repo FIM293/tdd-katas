4438d96ee095c5a76d73e5b1504fdeaf477e194e
tomastoniut01 <tomytoniut@gmail.com>
2024-09-20 10:07:19 -0300

    feat[StringCalculator]: segunda condición falla

--- typescript/src/tomas_toniut/StringCalculator.test.ts
+++ typescript/src/tomas_toniut/StringCalculator.test.ts
@@ -1,9 +1,38 @@
 import { StringCalculator } from "./StringCalculator";
+/*
+1. An empty string returns zero `'' => 0`
 
+2. A single number returns the value `'1' => 1` `'2' => 2`
+
+3. Two numbers, comma delimited, returns the sum `'1,2' => 3` `'10,20' => 30`
+
+4. Two numbers, newline delimited, returns the sum `'1\n2' => 3`
+
+5. Three numbers, delimited either way, returns the sum `'1\n2,3\n4' => 10`
+
+6. Negative numbers throw an exception with the message `'-1,2,-3' => 'negatives not allowed: -1,-3'`
+
+   **stop here if you are a beginner. Continue if you can finish the steps so far in less than 30 minutes.**
+
+7. Numbers greater than 1000 are ignored
+
+8. A single char delimiter can be defined on the first line starting with `//` (e.g `//#\n1#2` for a ‘#’ as the delimiter)
+
+9. A multi char delimiter can be defined on the first line starting with `//` (e.g. `//###\n1###2` for ‘###’ as the delimiter)
+
+*/
 describe(StringCalculator, () => {
-  it("should return 0 when the input is an empty string", () => {
+  it("debe retornar cero, si la entrada es una cadena vacía", () => {
     const stringCalculator = new StringCalculator();
     const result = stringCalculator.add('');
     expect(result).toEqual(0);
   });
+
+  it("debe retornar un numero simple, si la entrada es un numero simple", () => {
+    const stringCalculator = new StringCalculator();
+    const result = stringCalculator.add('15');
+    expect(result).toEqual(15);
+  });
+
+
 });
