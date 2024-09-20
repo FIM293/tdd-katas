834ca44fd2f7272f8df7a057e5bbd194f925695a
tomastoniut01 <tomytoniut@gmail.com>
2024-09-20 12:20:10 -0300

    feat[PrimeFactors]: el cuarto test ha pasado

--- typescript/src/tomas_toniut/PrimeFactors.ts
+++ typescript/src/tomas_toniut/PrimeFactors.ts
@@ -10,6 +10,14 @@ export class PrimeFactors {
       factores = [2];
     else if (number === 3)
       factores = [3];
+    else {
+      let divisor = 2;
+      while (number % divisor === 0) {
+        factores.push(divisor);
+        number = number / divisor;
+      }
+
+    }
     return factores;
   }
 }
