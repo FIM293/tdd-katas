ea4c66c95952989a6558fc84244271e11cdb5cef
Lucio Gabriel Paulino <Lucio@sols-MacBook-Pro.local>
2024-09-22 11:34:20 -0300

    Feat [Paso 2]: <Falla test>

--- typescript/src/lucio/PrimeFactors.test.ts
+++ typescript/src/lucio/PrimeFactors.test.ts
@@ -6,4 +6,9 @@ describe(PrimeFactors, () => {
     const result = sut.generate(1);
     expect(result).toEqual([]);
   });
+  it("should return [2] when number is 2", () => {
+    const sut = new PrimeFactors();
+    const result = sut.generate(2);
+    expect(result).toEqual([2]);
+  });
 });
