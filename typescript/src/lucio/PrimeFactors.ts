5ac1f74fa365dd4c5dfdcdb74cefaca81167cef3
Lucio Gabriel Paulino <Lucio@sols-MacBook-Pro.local>
2024-09-22 17:58:08 -0300

    Feat [Refactorizacion]: <Optimiza el codigo para que funciones para todos los casos (no solo para los casos de los test)>

--- typescript/src/lucio/PrimeFactors.ts
+++ typescript/src/lucio/PrimeFactors.ts
@@ -2,26 +2,35 @@ export class PrimeFactors {
   constructor() {}
 
   generate(number) {
-    if(number == 1)
-      return [];
-    if(number == 2)
-      return [2]
-    if(number == 3)
-      return [3]
-    if(number == 4)
-      return [2,2]
-    if(number == 5)
-      return [5]
-    if(number == 6)
-      return [2,3]
-    if(number == 7)
-      return [7]
-    if(number == 8)
-      return [2,2,2]
-    if(number == 9)
-      return [3,3]
-    if(number == 4620)
-      return [2,2,3,5,7,11]
+    //Si es primo retorno el numero
+    if(this.isPrime(number))
+      return [number];
+    let res = [];
 
+    // Iterar sobre todos los posibles factores primos
+    for (let i = 2; i <= number; i++) {
+      // Mientras el número sea divisible por 'i', es un factor primo
+      while (number % i === 0) {
+        res.push(i); // Añadir el factor primo
+        number = number / i; // Reducir el número
+      }
+    }
+
+    return res;
+  }
+
+  isPrime(number){
+    if (number <= 1) {
+      return false;
+    }
+  
+    // Solo necesitamos verificar hasta la raíz cuadrada de 'number'
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false; // Si es divisible por cualquier número, no es primo
+      }
+    }
+  
+    return true; // Si no es divisible por ningún número, es primo
   }
 }
