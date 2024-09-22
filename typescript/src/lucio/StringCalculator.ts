514759d92cb5ce8d987d76c374b4c42e482318bc
Lucio Gabriel Paulino <Lucio@sols-MacBook-Pro.local>
2024-09-22 10:37:44 -0300

    Feat [Paso 2]: <Pasa test>

--- typescript/src/lucio/StringCalculator.ts
+++ typescript/src/lucio/StringCalculator.ts
@@ -2,8 +2,12 @@ export class StringCalculator {
   constructor() {}
 
   add(input) {
+    //An empty string returns zero
     if(input == "")
-      return 0;  
+      return 0;
+    //A single number returns the value '1' => 1 '2' => 2
+    if(Number(input))  
+      return Number(input);
     return null;
   }
 }
