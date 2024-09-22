857620eb2c2395acdac49a5eb9c4ca46dc99dfcb
Lucio Gabriel Paulino <Lucio@sols-MacBook-Pro.local>
2024-09-22 10:56:47 -0300

    Feat [Paso 4]: <Pasa test>

--- typescript/src/lucio/StringCalculator.ts
+++ typescript/src/lucio/StringCalculator.ts
@@ -11,6 +11,9 @@ export class StringCalculator {
     //Two numbers, comma delimited, returns the sum '1,2' => 3 '10,20' => 30
     if(input.includes(","))
       return input.split(",").map(Number).reduce((acc, num) => acc + num, 0);
+    // Two numbers, newline delimited, returns the sum '1\n2' => 3
+    if(input.includes("/n"))
+      return input.split("/n").map(Number).reduce((acc, num) => acc + num, 0);
     return null;
   }
 }
